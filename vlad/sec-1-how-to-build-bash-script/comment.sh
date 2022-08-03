10b9ae94186df64349d55805a1047c8892c73200
victorkolis <victorkolis@gmail.com>
2022-08-03 12:47:35 -0300

    restructure

---
+++ vlad/sec-1-how-to-build-bash-script/comment.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# Author: Victor Kolis
+# Date Created: 21/08/2022
+# Last Modified: 21/08/2022
+
+# Description
+# Prints some text out to the terminal
+
+# Usage
+# comment
+
+
+# This is an inline comment
+# Comments are for humans only
+
+clear
+echo "Type in your name: "
+read name
+echo "Hi there, $name!"
+
